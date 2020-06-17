5486f1bcfd18f1911d65cb482a7791645d727a61
altryx <dlampa@gmail.com>
2020-06-16 19:16:26 -0600

    Comments added in source for Bug #001

--- HelloWorldUserEB/HelloWorldUserEB/Program.cs
+++ HelloWorldUserEB/HelloWorldUserEB/Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorldUserEB
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Refer to example on https://docs.microsoft.com/en-us/dotnet/api/system.console.readkey?view=netcore-3.1
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
     }
