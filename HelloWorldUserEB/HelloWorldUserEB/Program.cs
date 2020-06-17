9d3cb8b618e2f492f1a47b9fa2d05365eea3b78e
altryx <dlampa@gmail.com>
2020-06-16 23:39:14 -0600

    Update containing unit tests.

--- HelloWorldUserEB/HelloWorldUserEB/Program.cs
+++ HelloWorldUserEB/HelloWorldUserEB/Program.cs
@@ -2,14 +2,23 @@
 
 namespace HelloWorldUserEB
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static ConsoleKey keyPressed;
+
+        public static void Main()
         {
             Console.WriteLine("Hello World!");
 
-            // Refer to example on https://docs.microsoft.com/en-us/dotnet/api/system.console.readkey?view=netcore-3.1
-            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            while (keyPressed != ConsoleKey.Enter) 
+            {
+                Console.WriteLine("\nPress the Enter key to end this program...");
+                // Citation:
+                // Refer to example on https://docs.microsoft.com/en-us/dotnet/api/system.console.readkey?view=netcore-3.1 (ReadKey() method)
+                // and https://docs.microsoft.com/en-us/dotnet/api/system.consolekeyinfo.key?view=netcore-3.1#System_ConsoleKeyInfo_Key (ConsoleKeyInfo.Key property) 
+                // Reads the keypress by the user (using the ReadKey() method) and saves the ConsoleKeyInfo object's Key property value into the variable keyPressed (var type ConsoleKey)
+                keyPressed = Console.ReadKey().Key;
+            }
         }
     }
 }
